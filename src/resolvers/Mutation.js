10c2d2263cd5c24e4454e314c1163417abb49600
Danny Vu <danny.hvu@gmail.com>
2019-05-16 11:46:49 +0200

    Add reset token and reset password mutations and expose them

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { randomBytes } = require('crypto');
+const { promisify } = require('util');
 
 const mutations = {
   async createItem(parent, args, ctx, info) {
@@ -83,6 +85,62 @@ const mutations = {
   logout(parent, args, ctx, info) {
     ctx.response.clearCookie('token');
     return { message: 'Logged Out' };
+  },
+
+  async requestReset(parent, args, ctx, info) {
+    const user = await ctx.db.query.user({ where: { email: args.email } });
+    if (!user) {
+      return {
+        message: "Follow the instructions we've sent to your email address",
+      };
+    }
+
+    const resetToken = (await promisify(randomBytes)(20)).toString('hex');
+    const resetTokenExpiry = Date.now() + 3600000; // 1 hour from now
+    const res = await ctx.db.mutation.updateUser({
+      where: { email: args.email },
+      data: { resetToken, resetTokenExpiry },
+    });
+
+    return {
+      message: "Follow the instructions we've sent to your email address",
+    };
+  },
+
+  async resetPassword(parent, args, ctx, info) {
+    if (args.password !== args.confirmPassword) {
+      throw new Error("Passwords don't match");
+    }
+
+    const [user] = await ctx.db.query.users({
+      where: {
+        resetToken: args.resetToken,
+        resetTokenExpiry_gte: Date.now() - 3600000,
+      },
+    });
+    if (!user) {
+      throw new Error('Invalid/expired token');
+    }
+
+    const password = await bcrypt.hash(args.password, 10);
+
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: { email: user.email },
+      data: {
+        password,
+        resetToken: null,
+        resetTokenExpiry: null,
+      },
+    });
+
+    const token = jwt.sign({ userId: updatedUser }, process.env.APP_SECRET);
+
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week coookie
+    });
+
+    return updatedUser;
   },
 };
 
